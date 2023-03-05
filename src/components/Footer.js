f87e7f6aa3ce314d8b93d099ebf687b3b9607b60
anucak <56074636+anucak@users.noreply.github.com>
2023-03-05 19:02:26 +0100

    update year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="footer text-center">
-            <p>Made with ❤️ in Zagreb © 2022 Splitco</p>
+            <p>Made with ❤️ in Zagreb © 2023 Splitco</p>
         </footer> 
     );
 } 
